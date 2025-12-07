c990d186ab951703f9907aa6f7bb59585f17ad0e
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2025-12-07 23:28:18 +0100

    add repository pattern

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -4,4 +4,6 @@ import "net/http"
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
+
+	app.store.Posts.Create(r.Context())
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/tomidziurdzia/social/internal/env"
+	"github.com/tomidziurdzia/social/internal/store"
 )
 
 func main() {
@@ -11,9 +12,13 @@ func main() {
 		addr: env.GetString("ADDR", ":8080"),
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store: store,
 	}
+
 
 	mux := app.mount()
 	log.Fatal(app.run(mux))
